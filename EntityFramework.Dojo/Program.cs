7c94b5da237ef6afd23cd738656f2ed24bc7a610
Styrna <piotr.styrna@solarwinds.com>
2017-03-25 14:58:12 +0100

    init

---
+++ EntityFramework.Dojo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Dojo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
