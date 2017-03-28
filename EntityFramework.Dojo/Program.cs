b2565d93b9d50fc5899eb9e29d39ea2a4333c2f1
pstyrna <p.styrna@gmail.com>
2017-03-28 11:38:15 +0200

    change to localDB file

--- EntityFramework.Dojo/Program.cs
+++ EntityFramework.Dojo/Program.cs
@@ -21,52 +21,60 @@ namespace EntityFramework.Dojo
             {
                 context.Database.Log = (message) => _log.Debug(message);
 
+                //---------------------------------Add to table
                 //var school = new School
                 //{
-                //    Name = "90",
-                //    Adress = "Piastow"
+                //    Name = "89",
+                //    Adress = "Szkolne"
                 //};
 
-                //var clas1a = new Class
+                //var class1a = new Class
                 //{
                 //    Name = "1a",
-                //    SchoolId = 2
+                //    SchoolId = 1
                 //};
-                //var clas1b = new Class
+                //var class1b = new Class
                 //{
                 //    Name = "1b",
-                //    SchoolId = 2
+                //    SchoolId = 1
                 //};
 
-
                 //context.Schools.Add(school);
-                //context.Classes.AddRange(new[] { clas1a, clas1b });
+                //_log.Info(school.Name);
+                //_log.Info(school.Adress);
+                //context.Classes.AddRange(new[] { class1a, class1b });
+                //_log.Info(class1a.Name);
+                //_log.Info(class1a.SchoolId);
+                //_log.Info(class1b.Name);
+                //_log.Info(class1b.SchoolId);
+                //context.SaveChanges();
 
-                var school = new School()
-                {
-                    Id = 2
-                };
+                //-----------------------------Attach to value in table
+                //var school = new School()
+                //{
+                //    Id = 1
+                //};
 
-                context.Schools.Attach(school);
-                context.Schools.Remove(school);
+                //context.Schools.Attach(school);
+                //context.Schools.Remove(school);
+                //context.SaveChanges();
 
-                context.SaveChanges();
+                //----------------------------------------------------Select
+                //var result = context.Schools.Select(s => new
+                //{
+                //    SchoolName = s.Name,
+                //    ClassesNames = s.Classes.Select(c => c.Name)
+                //});
 
-                var result = context.Schools.Select(s => new
-                {
-                    SchoolName = s.Name,
-                    ClassesNames = s.Classes.Select(c => c.Name)
-                });
-                
 
-                foreach (var s in result)
-                {
-                    _log.Info(s.SchoolName);
-                    foreach (var c in s.ClassesNames)
-                    {
-                        _log.Info($"   {c}");
-                    }
-                }
+                //foreach (var s in result)
+                //{
+                //    _log.Info(s.SchoolName);
+                //    foreach (var c in s.ClassesNames)
+                //    {
+                //        _log.Info($"   {c}");
+                //    }
+                //}
             }
             _log.Debug("End");
             Console.ReadKey();
